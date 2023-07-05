8baaeb9590cf894f62b9bb414cd4bc541fdd90d4
ChanHo <tareun3406@gmail.com>
2023-07-05 16:39:07 +0900

    add login page in frontend

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form class="login-form" v-on:submit="onSubmitTest">
+    <p>ID <input type="text" name="username"></p>
+    <p>PW <input type="password" name="password"></p>
+
+    <button>로그인</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'LoginForm',
+    methods : {
+      onSubmitTest(event)
+      {
+        event.preventDefault();
+        console.log("test");
+      }
+    }
+  }
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <login-form></login-form>
+  </div>
+</template>
+
+<script>
+  import LoginForm from "@/components/LoginForm.vue";
+
+  export default {
+    name: 'LoginView',
+    components: {
+      LoginForm
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
