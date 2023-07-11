4efaae8962452183ed405ed2ad74950515e6e0b6
ChanHo <tareun3406@gmail.com>
2023-07-11 11:30:26 +0900

    fix : pw invalid message in mypage

--- frontend/src/components/MyPage/PasswordInput.vue
+++ frontend/src/components/MyPage/PasswordInput.vue
@@ -24,7 +24,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters("joinForm",["getIsSamePw"]),
+    ...mapGetters("myPageForm",["getIsSamePw"]),
   },
   watch: {
     localPassword(value) {
