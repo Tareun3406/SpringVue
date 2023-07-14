0ef728237fa7a27d5310c0901d2b7b1386ec97d0
ChanHo <tareun3406@gmail.com>
2023-07-14 13:04:28 +0900

    add : add validate check in posting board api

--- backend/src/main/java/kr/tareun/practice/controller/BoardController.java
+++ backend/src/main/java/kr/tareun/practice/controller/BoardController.java
@@ -6,6 +6,8 @@ import kr.tareun.practice.vo.BoardPostVO;
 import kr.tareun.practice.vo.BoardVO;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -24,13 +26,16 @@ public class BoardController {
     }
 
     @PostMapping("/board")
-    public void postBoard(@RequestBody BoardPostVO post, Principal principal, HttpServletResponse response) {
+    public ResponseEntity<String> postBoard(@RequestBody BoardPostVO post, Principal principal, HttpServletResponse response) {
 
         if (principal == null) {
-            response.setStatus(Response.SC_UNAUTHORIZED);
-            return;
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요합니다.");
+        } else if (post.getTitle() == null || post.getContent() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("제목과 내용을 모두 입력해야 합니다.");
         }
+
         boardService.insertBoard(new BoardVO(principal.getName(), post.getTitle(), post.getContent()));
+        return ResponseEntity.ok("게시물이 등록 되었습니다.");
     }
 
     @GetMapping("/board")
